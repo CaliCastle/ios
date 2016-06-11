85ff5224bdb7689a0f77fdfcd82737d9fa04c044
Cali Castle <cali@calicastle.com>
2016-06-11 19:55:50 +0800

    Complete iPhone 6 device mockup

---
+++ resources/views/layouts/ios.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>iOS 9网页模拟</title>
+
+    <link href="//cdn.bootcss.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ url('css/app.css') }}">
+</head>
+<body>
+
+    <main class="Main Presentation">
+        <div class="iPhone__wrapper">
+            <section class="iPhone iPhone--reflection iPhone--black">
+                <div class="iPhone__front">
+                    <div class="iPhone__top">
+                        <div class="Flex">
+                            <div class="iPhone__sensor"></div>
+                        </div>
+                        <div class="Flex Flex--horizontal">
+                            <div class="iPhone__camera"></div>
+                            <div class="iPhone__microphone"></div>
+                        </div>
+                    </div>
+                    <div class="iPhone__screen"></div>
+                    <div class="iPhone__home">
+                        <div class="iPhone__home__outer"></div>
+                        <div class="iPhone__home__inner"></div>
+                    </div>
+                </div>
+            </section>
+            <div class="iPhone__side">
+                <div class="iPhone__stripes">
+                    <div class="iPhone__stripe"></div>
+                    <div class="iPhone__stripe"></div>
+                    <div class="iPhone__stripe"></div>
+                    <div class="iPhone__stripe"></div>
+                </div>
+                <div class="iPhone__buttons">
+                    <div class="iPhone__silent"></div>
+                    <div class="iPhone__vol-up"></div>
+                    <div class="iPhone__vol-down"></div>
+                    <div class="iPhone__lock"></div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+</body>
+</html>
